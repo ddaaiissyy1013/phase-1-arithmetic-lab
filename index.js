b213ab92b0675da721f50ef741c44c9b72ec87cf
Daisy Sanchez <ddaaiissyy1013@gmail.com>
2023-12-12 10:06:12 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(11, 12, 13, 19, 20);
+console.log(multiply);
+console.log(mod);
+console.log(random);
+console.log(max);
